768f21fa8d697342c7e2a59977840eaaab7b73a3
meta-komal-khatri <41181810+meta-komal-khatri@users.noreply.github.com>
2018-08-02 16:19:26 +0530

    Add files via upload

--- PF/PF-Assignment-1/Cart.java
+++ PF/PF-Assignment-1/Cart.java
@@ -1,9 +1,28 @@
 package shopping;
 import java.util.*;
 public class Cart {
+	FixedProductPromotion fixedProductPromotion;
+	public Cart() {
+		fixedProductPromotion=new FixedProductPromotion();
+		fixedProductPromotion.setMinimumPriceOfProductForPromotion(1,100);
+		fixedProductPromotion.setMinimumPriceOfProductForPromotion(2,40);
+		fixedProductPromotion.setMinimumPriceOfProductForPromotion(3,700);
+		fixedProductPromotion.setMinimumPriceOfProductForPromotion(6,150);
+		fixedProductPromotion.setMinimumDiscount(1,10);
+		fixedProductPromotion.setMinimumDiscount(2,5);
+		fixedProductPromotion.setMinimumDiscount(3,20);
+		fixedProductPromotion.setMinimumDiscount(6,10);
+	}
+	Promotion promotion;
 	public static List<Product> cartproductlist = new ArrayList<Product>();
 	public void addProductCart(int id,int quantity) {
-		cartproductlist.add(new Product(id,Store.storeproductlist.get(Store.getIndexStore(id)).getProductName(),Store.storeproductlist.get(Store.getIndexStore(id)).getProductPrice(),quantity));
+		int discount=0;
+		System.out.println(fixedProductPromotion.getMinimumPriceOfProductPromotion(id));
+		int price=(Store.storeproductlist.get(Store.getIndexStore(id)).getProductPrice())*quantity;
+		if(fixedProductPromotion.getMinimumPriceOfProductPromotion(id)<=price) {
+			discount=fixedProductPromotion.getMinimumDiscount(id);
+		}
+		cartproductlist.add(new Product(id,Store.storeproductlist.get(Store.getIndexStore(id)).getProductName(),Store.storeproductlist.get(Store.getIndexStore(id)).getProductPrice(),quantity,discount));
 		Store.LeftQuantity(id, quantity);
 	}
 	public void removeFromCart(int id,int quantity) {
@@ -24,18 +43,54 @@ public class Cart {
 	    System.out.println("\n" + "Item " + id + " wasn't found in the store	.");
 	    return -1;
 	}
-	public int generateBill() {
+	/**
+	 * this method generates the bill of the cart items and also applies the discount if applicable
+	 * @param promoCode is the promocode as provided by user
+	 */
+	public int generateBill(String promoCode) {
 		int totalamount = 0;
+		promotion=new FixedOrderPromotion();
 		for(Product s:cartproductlist) {
 			
-		totalamount+=(s.Product_Price)*(s.product_quantity);
+			int productTotalPrice=s.Product_Price*s.product_quantity;
+			if(s.product_discount>0) {
+				totalamount+=productTotalPrice-(((productTotalPrice*s.product_discount)/100));
+			}
+			else {
+				totalamount+=productTotalPrice;
+			}
 		}
+		if(!"N".equals(promoCode)) {
+			promotion.setMinimumPrice(20);
+			promotion.setFixedDiscount(10);
+			int discount = applyPromotion(promotion, promoCode, totalamount);
+			System.out.println("\nTotal:" + totalamount);
+			System.out.println("discount applied:"+discount+" %");
+			totalamount = totalamount - ((discount/100)*totalamount);
+			System.out.println("Grand Total:  " + totalamount);
+			}
 		return totalamount;
 	}
 	public void view() {
 		for(Product s:cartproductlist) {
 			   System.out.println(s.Product_Id + "\t" + s.Product_Name + "\t" + s.Product_Price + "\t" + s.product_quantity);
 		   }
+	}
+	/**
+	 * this method generates the discount based on the promotion applied 
+	 * @param promotion is an object of Promotion class
+	 * @param code is a string having the promocode
+	 * @param amount is the total amount of the items in the cart 
+	 * @return it returns the % discount that can be applied on the order
+	 */
+	public int applyPromotion(Promotion promotion, String code, int amount)
+	{
+		int discount = 0;
+		if(promotion.isPromotionApplicable(code) && amount >= promotion.getMinimumPrice())
+		{
+			discount = promotion.getFixedDiscount();
+		}
+		return discount;
 	}
 	
 }
